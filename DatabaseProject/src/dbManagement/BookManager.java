c29a9786841bddc4f13422ecf8e019cbfc5b9255
seifgneedy <seifgneedy2@gmail.com>
2022-01-09 11:56:05 +0200

    Id and sale price would be updated automatically

--- DatabaseProject/src/dbManagement/BookManager.java
+++ DatabaseProject/src/dbManagement/BookManager.java
@@ -1,8 +1,10 @@
 package dbManagement;
 import java.sql.*;
+import java.text.SimpleDateFormat;
 
 import entities.*;
 public class BookManager {
+	private SimpleDateFormat format = new  SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private PreparedStatement addBookStatement;
 	private PreparedStatement addSaleStatement;
 	private PreparedStatement addBookOrderStatement;
@@ -10,10 +12,12 @@ public class BookManager {
 	BookManager(Connection connection) throws SQLException{
 		addBookStatement = connection.prepareStatement("INSERT INTO BOOKS VALUES"
 								+"(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		addSaleStatement = connection.prepareStatement("INSERT INTO SALES VALUES"
-								+"(?, ?, ?, ?, ?, ?)");
-		addBookOrderStatement = connection.prepareStatement("INSERT INTO BOOK_ORDER VALUES"
-								+"(?, ?, ?, ?)");
+		addSaleStatement = connection.prepareStatement("INSERT INTO SALES"+
+								"(USER_NAME,ISBN,SALE_TIME,QUANTITY) VALUES"
+								+"( ?, ?, ?, ?)");
+		addBookOrderStatement = connection.prepareStatement("INSERT INTO BOOK_ORDER"+
+								"(ISBN,QUANTITY,ORDER_DATE) VALUES"
+								+"(?, ?, ?)");
 		confirmOrderStatement = connection.prepareStatement("DELETE FROM BOOK_ORDER WHERE ID = ?");
 	}
 	public boolean addBook(Book book) throws SQLException {
@@ -29,19 +33,16 @@ public class BookManager {
 		return addBookStatement.executeUpdate() == 1;
 	}
 	public boolean addSale(Sale sale) throws SQLException {
-		addSaleStatement.setInt(1, sale.getId()); // Should the id be generated in DB!
-		addSaleStatement.setString(2, sale.getUserName());
-		addSaleStatement.setString(3, sale.getIsbn());
-		addSaleStatement.setDate(4, sale.getSaleTime());
-		addSaleStatement.setInt(5, sale.getQuantity());
-		addSaleStatement.setDouble(6, sale.getSalePrice());
+		addSaleStatement.setString(1, sale.getUserName());
+		addSaleStatement.setString(2, sale.getIsbn());
+		addSaleStatement.setString(3, format.format(sale.getSaleTime()));
+		addSaleStatement.setInt(4, sale.getQuantity());
 		return addSaleStatement.executeUpdate() == 1;
 	}
 	public boolean addBookOrder(BookOrder order) throws SQLException {
-		addBookOrderStatement.setInt(1, order.getId());// Should the id be generated in DB!
-		addBookOrderStatement.setString(2, order.getIsbn());
-		addBookOrderStatement.setInt(3, order.getQuantity());
-		addBookOrderStatement.setDate(4, order.getOrderDate());
+		addBookOrderStatement.setString(1, order.getIsbn());
+		addBookOrderStatement.setInt(2, order.getQuantity());
+		addBookOrderStatement.setString(3, format.format(order.getOrderDate()));
 		return addBookOrderStatement.executeUpdate() == 1;
 	}
 	public boolean confirmBookOrder(int id) throws SQLException {
