6f8353b9cd9f27bb71cbafe9261a3f1528d6f39f
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-01-09 04:33:49 +0200

    Update module-info.java

--- DatabaseProject/src/module-info.java
+++ DatabaseProject/src/module-info.java
@@ -2,6 +2,6 @@ module DatabaseProject {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
-	requires mysql.connector.java;
+	// requires mysql.connector.java;
 	opens application to javafx.graphics, javafx.fxml;
 }
