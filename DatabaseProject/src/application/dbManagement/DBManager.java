f6ef10b32e302e71250ee7d9ca4de943a2155bc6
Ans Elnagar <ansgom3a@gmail.com>
2022-01-10 14:31:17 +0200

    Update DBManager.java

--- DatabaseProject/src/application/dbManagement/DBManager.java
+++ DatabaseProject/src/application/dbManagement/DBManager.java
@@ -1,15 +1,13 @@
 package application.dbManagement;
 import java.sql.*;
+
 public class DBManager {
 	BookManager bookManager;
 	UserManager userManager;
-	Connection connection;
+	Connection connection; 
 	public DBManager() throws SQLException {
-		//Start connection
-		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        System.out.println("before connection");
         connection =DriverManager.getConnection(
-        				"jdbc:mysql://127.0.0.1:3306/world","root","1234");
+        				"jdbc:mysql://127.0.0.1:3306/BOOK_STORE","admin","admin");
         bookManager = new BookManager(connection);
         userManager = new UserManager(connection);
         
@@ -23,22 +21,18 @@ public class DBManager {
 	public void commitAll() throws SQLException {
 		connection.commit();
 	}
-//	public static void main(String args[]){
-//        try{
-//            Class.forName("com.mysql.jdbc.Driver");
-//            System.out.println("before connection");
-//            Connection con=DriverManager.getConnection(
-//            "jdbc:mysql://127.0.0.1:3306/FSCHOOL","admin","admin");
-//
-//            System.out.println("before creating the statement");
-//            Statement stmt=con.createStatement();
-//            ResultSet rs=stmt.executeQuery("SELECT * FROM users");
-//            System.out.println("before getting data");
-//            while(rs.next())
-//            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-//            con.close();
-//        }catch(Exception e){ System.out.println(e);}  
-//    }  
+	/*public static void main(String args[]){
+        try{
+            System.out.println("before connection");
+            Connection con =DriverManager.getConnection(
+    				"jdbc:mysql://127.0.0.1:3306/BOOK_STORE","admin","admin");
+            JasperDesign design = JRXmlLoader.load("src/application/reports/top5Customers.jrxml");
+            JasperReport report = JasperCompileManager.compileReport(design);
+            JasperPrint jprint = JasperFillManager.fillReport(report, null, con);
+            JasperViewer.viewReport(jprint, false);
+            con.close();
+        }catch(Exception e){ System.out.println(e);}  
+    }  */
 	
 	
 }
