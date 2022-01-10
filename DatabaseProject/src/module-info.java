f253b56ecc1430678253b716f6fa07810d9a9892
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2022-01-10 17:44:29 +0200

    Fixing some bugs

--- DatabaseProject/src/module-info.java
+++ DatabaseProject/src/module-info.java
@@ -8,7 +8,9 @@ module DatabaseProject {
 	requires mysql.connector.java;
 	requires java.base;
 	requires jasperreports;
+	requires javafx.base;
 
 	opens application to javafx.graphics, javafx.fxml;
 	opens application.dbManagement to java.sql, mysql.connector.java, jasperreports;
+	opens application.entities to javafx.base;
 }
