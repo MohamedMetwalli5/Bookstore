04f1250562107f066cf15cc9e108959753eb0eb6
Ans Elnagar <ansgom3a@gmail.com>
2022-01-10 14:29:43 +0200

    Update module-info.java

--- DatabaseProject/src/module-info.java
+++ DatabaseProject/src/module-info.java
@@ -7,8 +7,8 @@ module DatabaseProject {
 	requires java.sql;
 	requires mysql.connector.java;
 	requires java.base;
-
+	requires jasperreports;
 
 	opens application to javafx.graphics, javafx.fxml;
-	opens application.dbManagement to java.sql, mysql.connector.java;
+	opens application.dbManagement to java.sql, mysql.connector.java, jasperreports;
 }
